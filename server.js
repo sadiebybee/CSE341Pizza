e8bd662cef96f13e7f2d06df2dfe6ab98fd22566
Jade Miller <1jaremil1@gmail.com>
2025-06-28 21:53:08 -0600

    CORS issue

--- server.js
+++ server.js
@@ -9,6 +9,11 @@ app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  
+  if (req.method === "OPTIONS") {
+    return res.sendStatus(200);
+  }
+  
   next();
 });
 

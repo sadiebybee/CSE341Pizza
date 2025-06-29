e5d66ba59c0765c833582e1f0c1c7c9d8221de46
Jade Miller <1jaremil1@gmail.com>
2025-06-28 21:58:27 -0600

    Added options to access control allow methods

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ app.use(express.json());
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
   
   if (req.method === "OPTIONS") {
     return res.sendStatus(200);

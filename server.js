05def4663447c3cfbd6bd6f52333a269f5a21493
Jade Miller <1jaremil1@gmail.com>
2025-06-28 21:45:09 -0600

    testing render and endpoint set up

--- server.js
+++ server.js
@@ -7,6 +7,8 @@ const routes = require("./routes");
 app.use(express.json());
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
   next();
 });
 

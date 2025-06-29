f9b84709c4229a53fa506eef9c175fc178660c48
Jade Miller <1jaremil1@gmail.com>
2025-06-28 22:04:14 -0600

    Access control accept authorization

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const routes = require("./routes");
 app.use(express.json());
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
   
   if (req.method === "OPTIONS") {

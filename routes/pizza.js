18780b0fd1a8d3e9cfd9372c31730ccf2afa5ee8
sadiebybee <byb22004@byui.edu>
2025-07-11 12:56:49 -0600

    Pizza Collection

--- routes/pizza.js
+++ routes/pizza.js
@@ -1,9 +1,18 @@
 const express = require("express");
 const pizzaRouter = express.Router();
 
-const { getAll, getSingle } = require("../controllers/pizza");
+const {
+  getAll,
+  getSingle,
+  createPizza,
+  updatePizza,
+  deletePizza,
+} = require("../controllers/pizza");
 
 pizzaRouter.get("/", getAll);
 pizzaRouter.get("/:id", getSingle);
+pizzaRouter.post("/", createPizza);
+pizzaRouter.put("/:id", updatePizza);
+pizzaRouter.delete("/:id", deletePizza);
 
 module.exports = pizzaRouter;

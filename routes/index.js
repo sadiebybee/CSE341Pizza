5e997333afdbe8588c4f3e451594fc37e8567032
schmitty4437 <schmitty4437@gmail.com>
2025-07-17 19:05:24 -0600

    favorites collection done

--- routes/index.js
+++ routes/index.js
@@ -4,9 +4,11 @@ const pizzaRouter = require('./pizza');
 const router = express.Router();
 const myController = require('../controllers/pizza');
 const reviewsRouter = require('./reviews');
+const favoritesRouter = require('./favorites');
 
 router.use('/pizza', pizzaRouter);
 router.use('/reviews', reviewsRouter);
+router.use('/favorites', favoritesRouter);
 router.use('/api-docs', apiDocs);
 
 module.exports = router;

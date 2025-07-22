c3a6137b43a83464536ea23474b5b3f4ac008838
Jade Miller <1jaremil1@gmail.com>
2025-07-22 17:56:12 -0600

    s

--- routes/auth.js
+++ routes/auth.js
@@ -29,13 +29,6 @@ routes.get('/start', (req, res) => {
   res.redirect('./start.html');
 });
 
-//LOG OUT
-// routes.get('/logout', (req, res) => {
-//   req.logout(() => {
-//     res.redirect('/');
-//   });
-// });
-
 routes.get('/logout', (req, res) => {
   req.logout(() => {
     res.redirect('/logout.html');
@@ -49,48 +42,6 @@ routes.get("/start.html", (req, res) => {
   }
   
   res.sendFile(path.resolve(__dirname, '..', 'start.html'));
-});
-
-// PAGES
-
-routes.get("/profile.html", (req, res) => {
-  if (!req.isAuthenticated()) {
-    return res.redirect('/');
-  }
-  
-  res.sendFile(path.resolve(__dirname, '..', 'profile.html'));
-});
-
-routes.get("/scan.html", (req, res) => {
-  if (!req.isAuthenticated()) {
-    return res.redirect('/');
-  }
-  
-  res.sendFile(path.resolve(__dirname, '..', 'scan.html'));
-});
-
-routes.get("/social.html", (req, res) => {
-  if (!req.isAuthenticated()) {
-    return res.redirect('/');
-  }
-  
-  res.sendFile(path.resolve(__dirname, '..', 'social.html'));
-});
-
-routes.get("/general.html", (req, res) => {
-  if (!req.isAuthenticated()) {
-    return res.redirect('/');
-  }
-  
-  res.sendFile(path.resolve(__dirname, '..', 'general.html'));
-});
-
-routes.get("/list.html", (req, res) => {
-  if (!req.isAuthenticated()) {
-    return res.redirect('/');
-  }
-  
-  res.sendFile(path.resolve(__dirname, '..', 'list.html'));
 });
 
 module.exports = routes;

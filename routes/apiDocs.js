ce6f0d19b3ce7f6f2922c9f3847c6287552a11bb
sadiebybee <byb22004@byui.edu>
2025-07-22 13:08:06 -0600

    PizzaCollection

--- routes/apiDocs.js
+++ routes/apiDocs.js
@@ -1,13 +1,12 @@
-const express = require('express');
+const express = require("express");
 const apiDocs = express.Router();
-const swaggerUi = require('swagger-ui-express');
-const swaggerDocument = require('../swagger.json');
+const swaggerUi = require("swagger-ui-express");
+const swaggerDocument = require("../swagger.json");
 
-
-apiDocs.use('/', swaggerUi.serve);
-apiDocs.get('/', (req, res, next) => {
-    res.setHeader('Content-Type', 'text/html');
-    swaggerUi.setup(swaggerDocument)(req, res, next);
+apiDocs.use("/", swaggerUi.serve);
+apiDocs.get("/", (req, res, next) => {
+  res.setHeader("Content-Type", "text/html");
+  swaggerUi.setup(swaggerDocument)(req, res, next);
 });
 
 module.exports = apiDocs;

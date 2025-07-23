37785f6616f2e1a3a45fb0603e1faaefed61258c
Matt Schmitt <schmitty4437@gmail.com>
2025-07-23 14:44:36 -0600

    changed local to render link for testing

--- swagger.js
+++ swagger.js
@@ -4,10 +4,10 @@ const doc = {
     title: 'Pizza Review App',
     description: 'Users can log in review pizzas and add favorites',
   },
-  host: "localhost:3000",
-  schemes: ["http"],
-  // host: 'cse341pizza.onrender.com',
-  // schemes: ['https'],
+  // host: "localhost:3000",
+  // schemes: ["http"],
+  host: 'cse341pizza.onrender.com',
+  schemes: ['https'],
 };
 const outputFile = './swagger.json';
 const endpointsFiles = ['./server.js'];
